562e438fb0a7ed0b1fde149dcee632524519cd3d
PROF <74806806+ProfThorfinn@users.noreply.github.com>
2024-12-24 20:29:13 +0200

    Update and rename DrawableTest.java to Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,14 @@
-package mycompany.oop_drmaged3skr;
-
-public class DrawableTest {
+public class Main {
     public static void main(String[] args) {
         if (args.length < 3 || (args.length - 1) % 2 != 0) {
-            System.out.println("Example: DrawableTest 2 circle 22.5 cube 23.6");
+            System.out.println("Ex: DrawableTest 2 circle 22.5 cube 23.6");
             return;
         }
-
-        try {
             int arraySize = Integer.parseInt(args[0]);
             if (arraySize < 2) {
                 System.out.println("Array size must be at least 2.");
                 return;
             }
-
             Drawable[] drawables = new Drawable[arraySize];
             int index = 0;
 
@@ -22,36 +17,28 @@ public class DrawableTest {
                     System.out.println("Extra shapes ignored.");
                     break;
                 }
-
                 String type = args[i];
                 double size = Double.parseDouble(args[i + 1]);
 
                 if (type.equalsIgnoreCase("circle")) {
-                    drawables[index++] = new Circle("white", size);
+                    drawables[index++] = new Circle(size);
                 } else if (type.equalsIgnoreCase("cube")) {
-                    drawables[index++] = new Cube("white", size);
+                    drawables[index++] = new Cube(size);
                 } else {
                     System.out.println("Invalid shape type: " + type + ". Use 'circle' or 'cube'.");
                 }
             }
-
             double totalArea = 0;
-            System.out.println("Shapes List:");
+            // System.out.println("Shapes List:");
             for (Drawable drawable : drawables) {
                 if (drawable != null) {
                     if (drawable instanceof Shape) {
                         totalArea += ((Shape) drawable).getArea();
-                        System.out.println(drawable.toString());
+//                      System.out.println(drawable.toString());
                     }
                 }
             }
 
             System.out.println("\nTotal Area of Drawable Array: " + totalArea);
-
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid input.");
-        } catch (Exception e) {
-            System.out.println("An error occurred: " + e.getMessage());
-        }
     }
 }
